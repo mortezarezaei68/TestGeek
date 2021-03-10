2192de358030acd6ac6e885ad66269d1ceb569a8
mortezarezaei <morteza.rezaei68@gmail.com>
2021-03-10 11:17:29 +0330

    changes

--- src/Presentation/WebApp/wwwroot/js/MyGame.js
+++ src/Presentation/WebApp/wwwroot/js/MyGame.js
@@ -3,28 +3,39 @@
 function myFunction() {
     const firstName = document.getElementById("firstName").value;
     const lastName = document.getElementById("lastName").value;
-    const jData={
+    const jData = {
         lastName: firstName,
         firstName: lastName,
-        points:[]
+        points: []
     };
     $.ajax({
         type: "POST",
-        url: "https://localhost:5001/api/v1/user"
+        url: "/api/v1/user"
         , dataType: 'json'
-        ,contentType: "application/json; charset=utf-8",
+        , contentType: "application/json; charset=utf-8",
         data: JSON.stringify(jData)
-        ,success: function(data) {
+        , success: function (data) {
             if (localStorage.getItem("userId"))
                 localStorage.clear();
             localStorage.setItem("userId", data);
-            const y = document.getElementById("div1");
-            y.style.visibility = 'hidden';
-            const x = document.getElementById("div2");
-            x.style.visibility = 'visible'; 
+            document.getElementById("loginPanel").style.display = 'none';
+            document.getElementById("gameBoard").style.display = 'block';
+            
         },
         error: function (x, err, desc) {
             alert(desc);
         }
     })
 }
+
+
+
+
+document.getElementById('myDIV').onclick = function clickEvent(e) {
+    // e = Mouse click event.
+    var rect = e.target.getBoundingClientRect();
+    var x = e.clientX - rect.left; //x position within the element.
+    var y = e.clientY - rect.top;  //y position within the element.
+    console.log("Left? : " + x + " ; Top? : " + y + ".");
+}
+
